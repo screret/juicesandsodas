d4ad6258d302c2f11416b9dac5bd7a9355e08ee8
screret <68943070+screret@users.noreply.github.com>
2021-02-10 12:44:05 +0200

    initial commit

--- src/main/java/io/screret/github/juicesandsodas/init/ModItems.java
+++ src/main/java/io/screret/github/juicesandsodas/init/ModItems.java
@@ -8,26 +8,16 @@ import net.minecraftforge.registries.ObjectHolder;
 public class ModItems {
 
         //normies
-        @ObjectHolder("kool_aid")
         public static final Item KOOL_AID               = null;
-        @ObjectHolder("jello")
         public static final Item JELLO                  = null;
-        @ObjectHolder("kool_aid_empty")
         public static final Item KOOL_AID_EMPTY         = null;
-        @ObjectHolder("magic_aid")
         public static final Item MAGIC_AID              = null;
-        @ObjectHolder("lemonade")
         public static final Item LEMONADE               = null;
-        @ObjectHolder("lemonade_empty")
         public static final Item LEMONADE_EMPTY         = null;
 
         //armor
-        @ObjectHolder("juice_helmet")
         public static final Item JUICE_HELMET           = null;
-        @ObjectHolder("juice_chestplate")
         public static final Item JUICE_CHESTPLATE       = null;
-        @ObjectHolder("juice_leggings")
         public static final Item JUICE_LEGGINGS         = null;
-        @ObjectHolder("juice_boots")
         public static final Item JUICE_BOOTS            = null;
 }
