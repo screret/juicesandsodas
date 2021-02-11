2f741fb905c4cde6369889e64df929837cfdc844
screret <68943070+screret@users.noreply.github.com>
2021-02-11 14:16:56 +0200

    Update LemonTree.java

--- src/main/java/io/screret/github/juicesandsodas/plants/LemonTree.java
+++ src/main/java/io/screret/github/juicesandsodas/plants/LemonTree.java
@@ -12,18 +12,10 @@ import java.util.Random;
 
 public class LemonTree extends Tree {
 
-    BaseTreeFeatureConfig config = new BaseTreeFeatureConfig.Builder(
-            new SimpleBlockStateProvider(Blocks.OAK_LOG.getDefaultState()),
-            new SimpleBlockStateProvider(ModBlocks.LEMON_LEAVES.getDefaultState()),
-            new BlobFoliagePlacer(FeatureSpread.func_242252_a(2), FeatureSpread.func_242252_a(0), 3),
-            new StraightTrunkPlacer(4, 2, 0),
-            new TwoLayerFeature(1, 0, 1))
-            .setIgnoreVines()
-                .build();
-
-    @org.jetbrains.annotations.Nullable
-    @Override
-    protected ConfiguredFeature<BaseTreeFeatureConfig, ?> getTreeFeature(Random randomIn, boolean largeHive) {
-        return new ConfiguredFeature<BaseTreeFeatureConfig, Feature<BaseTreeFeatureConfig>>(TreeFeature.TREE, config);
-    }
+        
+   @Override
+   protected Feature<? extends BaseTreeFeatureConfig> getFeature(Random random)
+   {
+      return (random.nextInt(10) == 0 ? BOPFeatures.BIG_FLOWERING_OAK_TREE : BOPFeatures.FLOWERING_OAK_TREE);
+   }
 }
